e9812ef24216a5983c80da60beef962064bcb67b
Veera <164357439+vra0613@users.noreply.github.com>
2024-04-16 07:24:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "my-s3-bucket-for-tf-statefile"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "my-dynamo-db-table-tf"
   }
 }
